65d2626b1002ea8070acd9a6df3b79525ef2f87b
s1thu <s1thu.w1n.5601@gmail.com>
2025-03-02 19:26:35 +0630

    update : update with DTOs

---
+++ backend/app/model/Product/ProductDTO.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models\Product;
+
+class ProductDTO {
+    private int $id;
+    private string $name;
+    private float $price;
+    private string $image;
+
+    public function __construct(int $id, string $name, float $price, string $image) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+        $this->image = $image;
+    }
+
+    public function getId(): int { return $this->id; }
+    public function getName(): string { return $this->name; }
+    public function getPrice(): float { return $this->price; }
+    public function getImage(): string { return $this->image; }
+
+}
